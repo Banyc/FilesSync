fd60ae70f4a5d0e3d41d64bf6904beb5cb37ebdb
Banyc <36535895+Banyc@users.noreply.github.com>
2021-07-13 22:01:09 +0800

    feat: re-scan local file system at SSH error

--- src/FilesSync.Core/SshFilesSync.cs
+++ src/FilesSync.Core/SshFilesSync.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Threading.Tasks;
 using FilesSync.Core.Helpers;
 using FilesSync.Core.Models;
 
@@ -8,6 +9,10 @@ namespace FilesSync.Core
     {
         private readonly FilesChangeMonitor monitor;
         private readonly SshFilesSender sender;
+        private bool isScanning = false;
+        private bool hasNewScanningRequest = false;
+
+        private Task scanningTask;
 
         public SshFilesSync(FilesChangeMonitor monitor, SshFilesSender sender)
         {
@@ -23,78 +28,116 @@ namespace FilesSync.Core
 
         public void UpdateFilesChangesDuringDownTime()
         {
-            ScanAndUpdate();
+            ScheduleNewScanning();
         }
 
         private void OnFileEvent(object sender, FileSystemEventArgs e)
         {
-            switch (e.ChangeType)
+            try
             {
-                case WatcherChangeTypes.Created:
-                    // exclude directories
-                    if (File.Exists(e.FullPath))
-                    {
-                        this.sender.Create(e.FullPath);
-                        this.monitor.CommitCreate(e.FullPath);
-                    }
-                    break;
-                case WatcherChangeTypes.Deleted:
-                    this.sender.Delete(e.FullPath);
-                    this.monitor.CommitDelete(e.FullPath);
-                    break;
-                case WatcherChangeTypes.Changed:
-                    // exclude directories
-                    if (File.Exists(e.FullPath))
-                    {
-                        this.sender.Create(e.FullPath);
-                        this.monitor.CommitCreate(e.FullPath);
-                    }
-                    break;
-                case WatcherChangeTypes.Renamed:
-                    RenamedEventArgs eventArgs = (RenamedEventArgs)e;
-                    this.sender.Move(eventArgs.OldFullPath, eventArgs.FullPath);
-                    if (File.Exists(e.FullPath))
-                    {
-                        this.monitor.CommitMoveFile(eventArgs.OldFullPath, eventArgs.FullPath);
-                    }
-                    else
-                    {
-                        this.monitor.CommitMoveDirectory(eventArgs.OldFullPath, eventArgs.FullPath);
-                    }
-                    break;
-                case WatcherChangeTypes.All:
-                    break;
-                default:
-                    break;
+                switch (e.ChangeType)
+                {
+                    case WatcherChangeTypes.Created:
+                        // exclude directories
+                        if (File.Exists(e.FullPath))
+                        {
+                            this.sender.Create(e.FullPath);
+                            this.monitor.CommitCreate(e.FullPath);
+                        }
+                        break;
+                    case WatcherChangeTypes.Deleted:
+                        this.sender.Delete(e.FullPath);
+                        this.monitor.CommitDelete(e.FullPath);
+                        break;
+                    case WatcherChangeTypes.Changed:
+                        // exclude directories
+                        if (File.Exists(e.FullPath))
+                        {
+                            this.sender.Create(e.FullPath);
+                            this.monitor.CommitCreate(e.FullPath);
+                        }
+                        break;
+                    case WatcherChangeTypes.Renamed:
+                        RenamedEventArgs eventArgs = (RenamedEventArgs)e;
+                        this.sender.Move(eventArgs.OldFullPath, eventArgs.FullPath);
+                        if (File.Exists(e.FullPath))
+                        {
+                            this.monitor.CommitMoveFile(eventArgs.OldFullPath, eventArgs.FullPath);
+                        }
+                        else
+                        {
+                            this.monitor.CommitMoveDirectory(eventArgs.OldFullPath, eventArgs.FullPath);
+                        }
+                        break;
+                    case WatcherChangeTypes.All:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Renci.SshNet.Common.SshException ex)
+            {
+                ScheduleNewScanning();
             }
         }
 
         private void OnWatcherError(object sender, ErrorEventArgs e)
         {
-            ScanAndUpdate();
+            ScheduleNewScanning();
+        }
+
+        private void ScheduleNewScanning()
+        {
+            lock (this.monitor)
+            {
+                if (this.isScanning)
+                {
+                    this.hasNewScanningRequest = true;
+                }
+                else
+                {
+                    this.isScanning = true;
+                    scanningTask = Task.Run(() => ScanAndUpdate());
+                }
+            }
         }
 
         private void ScanAndUpdate()
         {
+            this.isScanning = true;
             do
             {
-                var paths = this.monitor.GetChangedUnitsPaths();
-                foreach (var path in paths)
+                System.Diagnostics.Debug.WriteLine("[scanner] Begin scanning");
+                try
                 {
-                    if (File.Exists(path))
+                    this.hasNewScanningRequest = false;
+
+                    var paths = this.monitor.GetChangedUnitsPaths();
+                    foreach (var path in paths)
                     {
-                        this.sender.Create(path);
-                        this.monitor.CommitCreate(path);
-                    }
-                    else
-                    {
-                        this.sender.Delete(path);
-                        this.monitor.CommitDelete(path);
+                        if (File.Exists(path))
+                        {
+                            this.sender.Create(path);
+                            this.monitor.CommitCreate(path);
+                        }
+                        else
+                        {
+                            this.sender.Delete(path);
+                            this.monitor.CommitDelete(path);
+                        }
                     }
                 }
+                catch (Renci.SshNet.Common.SshException ex)
+                {
+                    this.hasNewScanningRequest = true;
+                }
+                System.Diagnostics.Debug.WriteLine("[scanner] End scanning");
             }
             // if new file event occurs during scanning, then the scanning result is not valid.
-            while (this.monitor.HasNewFileEventDuringScan);
+            while (this.monitor.HasNewFileEventDuringScan ||
+                   this.hasNewScanningRequest);
+
+            this.isScanning = false;
         }
     }
 }
